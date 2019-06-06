6e3b8e5b0ee77bf61cdec2e4e52a1ce5a23bc210
gjonespf <gjones.pf@gmail.com>
2019-06-06 17:14:03 +1200

    Test submodule grab

--- pre.ps1
+++ pre.ps1
@@ -5,6 +5,9 @@
 
 # Do your project specific pre stuff here
 
+# Need to ensure submodules first
+git submodule update
+
 ## === TWEAK - Create an initial version file, or build will fail first time ===
 function Create-InitialVersionFile {
     $versionPath = "$PSScriptRoot/Cake.Recipe/Cake.Recipe/Content/version.cake"
