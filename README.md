ee39e07bc10923a0821f15e4ffcad4b85c66a535
Gavin Jones <gjones@powerfarming.co.nz>
2019-09-10 16:17:36 +1200

    Handle localised specific tool versions

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # Cake.Recipe.PFBuild
 
-Current test build:
+```pwsh
+# Current test build:
 ./build.ps1 -target build
 
-Test Dotnet Core build:
-dotnet-cake setup.cake --target="build"
+# Test Dotnet Core build:
+pwsh ./build.dntool.ps1 -target init
+
+# Test Dotnet Core build:
+dotnet-cake setup.cake --target="build" --verbosity=diagnostic
+```
 
 
--- build.dntool.ps1
+++ build.dntool.ps1
@@ -6,29 +6,62 @@ Param(
     [string]$Target = "build",
     [string]$Configuration,
     [ValidateSet("Quiet", "Minimal", "Normal", "Verbose", "Diagnostic")]
-    [string]$Verbosity,
+    [string]$Verbosity="Minimal",
     [switch]$ShowDescription,
     [Alias("WhatIf", "Noop")]
     [switch]$DryRun,
     [switch]$Experimental,
     [version]$CakeVersion = '0.33.0',
+    [string]$DotnetToolPath = '.dotnet/tools/',
     [Parameter(Position=0,Mandatory=$false,ValueFromRemainingArguments=$true)]
     [string[]]$ScriptArgs
 )
 
-if(!($env:PATH -match ".dotnet") -and $IsLinux) {
-    if(Test-Path "~/.dotnet/tools") {
-        $toolsPath = (Resolve-Path "~/.dotnet/tools").Path
-        $env:PATH = $env:PATH + ":$toolsPath"
-    } else {
-        Write-Warning "Couldn't find dotnet core tools directory"
+$toolPathExists = Resolve-Path $DotnetToolPath -ErrorAction SilentlyContinue
+if(!($toolPathExists)) {
+    New-Item -ItemType Directory -Path $DotnetToolPath | Out-Null
+}
+
+function New-DotnetToolDefinition ($PackageId, $Version, $CommandName) {
+    [pscustomobject] @{
+        PackageId = $PackageId
+        Version = $Version
+        CommandName = $CommandName
     }
 }
 
-# TODO: Pull in specific version of cake listed in $CakeVersion
+# Check existing versions...
+$toollist = (dotnet tool list --tool-path $DotnetToolPath)
+$toolvers = ($toollist | Select-Object -Skip 2) | %{ $packageId, $version, $commandname = $_.Trim() -split '\s+'
+    [pscustomobject] @{
+        PackageId = $packageId
+        Version = $version
+        CommandName = $commandname
+    }
+}
+# TODO: Load from a json config as alternative?
+$defaultToolVers = @(
+    New-DotnetToolDefinition -PackageId "cake.tool" -Version $CakeVersion -CommandName "dotnet-cake"
+    New-DotnetToolDefinition -PackageId "gitversion.tool" -Version "5.0.2-beta1.51" -CommandName "dotnet-gitversion"
+)
+
+foreach($tool in $defaultToolVers) {
+    # New install
+    if(!($toolvers | ?{ $_.PackageId -eq $tool.PackageId })) {
+        Write-Information "Installing missing tool $($tool.PackageId)"
+        dotnet tool install $tool.PackageId --tool-path $DotnetToolPath --version $tool.Version
+    }
+    # Update version (update in dotnet sdk 3 :/)
+    if(($toolvers | ?{ $_.PackageId -eq $tool.PackageId -and $_.Version -ne $tool.Version })) {
+        Write-Information "Installing updated tool $($tool.PackageId)"
+        dotnet tool uninstall $tool.PackageId --tool-path $DotnetToolPath
+        dotnet tool install $tool.PackageId --tool-path $DotnetToolPath --version $tool.Version
+    }
+}
+
 if(Get-Command "dotnet-cake" -ErrorAction SilentlyContinue) {
     Write-Information "Running dotnet-cake"
-    dotnet-cake $Script --target=$Target
+    dotnet-cake $Script --target=$Target --verbosity=$Verbosity
 } else {
     Write-Error "Could not find dotnet-cake to run build script"
     Write-Information "Using PATH: $($env:PATH)"
